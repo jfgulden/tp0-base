7cb37991a9bc62d1d8a543c3e4cc969edeaae1e9
Juan Francisco <guldenjf@gmail.com>
2024-08-27 10:42:37 -0300

    improve python code to adhere to best practices

--- generador.py
+++ generador.py
@@ -1,24 +1,49 @@
 from sys import argv
 
-file = argv[1]
-clients = int(argv[2])
 
-for i in range(1, clients+1):
-    with open(file, 'a') as f:
-        client = f"""
-    client{i}:
-        container_name: client{i}
-        image: client:latest
-        entrypoint: /client
-        environment:
-        - CLI_ID={i}
-        - CLI_LOG_LEVEL=DEBUG
-        networks:
-        - testing_net
-        depends_on:
-        - server
+def generate_clients(output_file, clients_num):
+    try:
+        with open(output_file, 'a') as f:
+            for i in range(1, clients_num+1):
+                client_container = f"""
+  client{i}:
+    container_name: client{i}
+    image: client:latest
+    entrypoint: /client
+    environment:
+      - CLI_ID={i}
+      - CLI_LOG_LEVEL=DEBUG
+    networks:
+      - testing_net
+    depends_on:
+      - server
 """
-        f.write(client)
+                
+                f.write(client_container)
+
+    except Exception as e:
+        print(f"Error while trying to write file '{output_file}': {e}")
+        return 1
+    
+
+def main():
+    if len(argv) != 3:
+        print("Usage: python3 generador.py <file> <clients>")
+        return 1
+    
+    try:
+        clients_num = int(argv[2])
+        if clients_num < 1:
+            raise ValueError("Number of clients must be a positive integer")
+        
+    except ValueError as ve:
+        print(f"Error: {ve}")
+        return 1
 
     
-f.close()
+    output_file = argv[1]
+    generate_clients(output_file, clients_num)
+
+
+if __name__ == '__main__':
+    main()
