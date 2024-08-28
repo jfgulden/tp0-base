71af754565515266fea6480b0363a21bb3d9ac2c
Juan Francisco <guldenjf@gmail.com>
2024-08-28 19:13:24 -0300

    graceful shutdown del server

--- server/common/server.py
+++ server/common/server.py
@@ -1,6 +1,7 @@
 import socket
 import logging
-
+import signal
+import time
 
 class Server:
     def __init__(self, port, listen_backlog):
@@ -8,6 +9,7 @@ class Server:
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
+        self._is_running = True
 
     def run(self):
         """
@@ -18,11 +20,31 @@ class Server:
         finishes, servers starts to accept new connections again
         """
 
-        # TODO: Modify this program to handle signal to graceful shutdown
-        # the server
-        while True:
+        signal.signal(signal.SIGTERM, self.__handle_sigterm)
+
+        while self._is_running:
             client_sock = self.__accept_new_connection()
+            if client_sock is None or not self._is_running:
+                break
+
             self.__handle_client_connection(client_sock)
+
+    def __handle_sigterm(self, signum, frame):
+        """
+        Handle SIGTERM signal
+
+        Function that handles the SIGTERM signal. It closes the server
+        socket.
+        """
+
+        logging.info("action: socket_close | result: in_progress")
+        self._is_running = False
+        self._server_socket.shutdown(socket.SHUT_RDWR)
+        self._server_socket.close()
+        logging.info("action: socket_close | result: success")
+        time.sleep(1)
+
+
 
     def __handle_client_connection(self, client_sock):
         """
@@ -53,6 +75,12 @@ class Server:
 
         # Connection arrived
         logging.info('action: accept_connections | result: in_progress')
-        c, addr = self._server_socket.accept()
-        logging.info(f'action: accept_connections | result: success | ip: {addr[0]}')
-        return c
+        try:
+            c, addr = self._server_socket.accept()
+            logging.info(f'action: accept_connections | result: success | ip: {addr[0]}')
+            return c
+        except OSError as e:
+            logging.error(f'action: accept_connections | result: fail | error: {e}')
+            return None
+        
+        
