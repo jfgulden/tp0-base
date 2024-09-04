19446f1498c700e5ec7ebc6b76a51d46e6f43881
Juan Francisco <guldenjf@gmail.com>
2024-09-04 20:10:14 -0300

    modify batch size in configf

--- client/main.go
+++ client/main.go
@@ -10,7 +10,6 @@ import (
 	"github.com/op/go-logging"
 	"github.com/pkg/errors"
 	"github.com/spf13/viper"
-
 	"github.com/7574-sistemas-distribuidos/docker-compose-init/client/common"
 )
 
@@ -82,12 +81,13 @@ func InitLogger(logLevel string) error {
 // PrintConfig Print all the configuration parameters of the program.
 // For debugging purposes only
 func PrintConfig(v *viper.Viper) {
-	log.Infof("action: config | result: success | client_id: %s | server_address: %s | loop_amount: %v | loop_period: %v | log_level: %s",
+	log.Infof("action: config | result: success | client_id: %s | server_address: %s | loop_amount: %v | loop_period: %v | log_level: %s | batch_max_amount: %v",
 		v.GetString("id"),
 		v.GetString("server.address"),
 		v.GetInt("loop.amount"),
 		v.GetDuration("loop.period"),
 		v.GetString("log.level"),
+		v.GetInt("batch.maxAmount"),
 	)
 }
 
