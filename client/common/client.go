b154c6a2a4336e0a3bf13187f6a9953203293a07
Juan Francisco <guldenjf@gmail.com>
2024-09-05 17:23:20 -0300

    client refactor

--- client/common/client.go
+++ client/common/client.go
@@ -127,80 +127,6 @@ func (c *Client) readMsg(length int) (string, error) {
 	return msg, nil
 }
 
-func (c *Client) serialize_batch(bets []Bet, eof_flag uint8) ([]byte, error) {
-	var buffer bytes.Buffer
-	for _, bet := range bets {
-		betBuffer, err := bet.serialize()
-		if err != nil {
-			log.Criticalf("action: serialize | result: fail | client_id: %v | error: %v",
-				c.config.ID,
-				err,
-			)
-			return nil, err
-		}
-		if _, err := buffer.Write(betBuffer); err != nil {
-			return nil, err
-		}
-	}
-	totalLength := uint8(len(bets))
-	var finalBuffer bytes.Buffer
-	binary.Write(&finalBuffer, binary.BigEndian, eof_flag)
-
-	if err := binary.Write(&finalBuffer, binary.BigEndian, totalLength); err != nil {
-        return nil, err
-    }
-
-	finalBuffer.Write(buffer.Bytes())
-	return finalBuffer.Bytes(), nil
-}
-
-func (c *Client) prepareBatchForSending(bets []Bet) ([]Bet, []byte, error) {
-
-	batch := chunkBets(bets, c.config.BatchMaxAmount)
-
-	buffer, err := c.serialize_batch(batch, func() uint8 {
-		if len(batch) == len(bets) {
-			return EOF_MSG_TRUE
-		}
-		return EOF_MSG_FALSE
-	}())
-		
-
-	if err != nil {
-		return batch, nil, err
-	}
-
-	for len(buffer) > BATCH_MAX_AMOUNT_BYTES {
-		batchSize := len(batch) * 3 / 4
-		if batchSize < 1 {
-			return nil, nil, fmt.Errorf("batch size too small to continue")
-		}
-		batch = chunkBets(bets, batchSize)
-		if len(batch) == len(bets) {
-			buffer, err = c.serialize_batch(batch, EOF_MSG_TRUE)
-		} else {	
-			buffer, err = c.serialize_batch(batch, EOF_MSG_FALSE)
-		}
-		if err != nil {
-			return batch, nil, err
-		}
-	}
-	return batch, buffer, nil
-}
-
-
-func chunkBets(bets []Bet, maxAmount int) []Bet {
-	batches := make([]Bet, 0, maxAmount)
-
-	if len(bets) <= maxAmount {
-
-		return bets
-	}
-	for i := 0; i < maxAmount; i++ {
-		batches = append(batches, bets[i])
-	}
-	return batches
-}
 func (c *Client) receiveWinners() ([]string, error) {	
 	var winners []string
 	msg, err := c.readAll(WINNERS_NUM_BYTES)
@@ -219,34 +145,7 @@ func (c *Client) receiveWinners() ([]string, error) {
 	winners = strings.Split(winners_buf, ",")
 	return winners, nil
 }
-func (c *Client) readBetsFromFile(csvReader *csv.Reader, maxAmount int) []Bet {
-	totalBytes := 0
-	var bets []Bet
-	
-	for {
-		record, err := csvReader.Read()
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			log.Errorf("action: read_csv | result: fail | client_id: %v | error: %v", c.config.ID, err)
-			return nil
-		}
 
-		recordStr := strings.Join(record, ",") + "\n"
-		recordBytes := len(recordStr)
-		if totalBytes+recordBytes > BATCH_MAX_AMOUNT_BYTES {
-			break
-		}
-
-		totalBytes += recordBytes
-		
-		bet := NewBet(c.config.ID, record[0], record[1], record[2], record[3], record[4])
-		bets = append(bets, *bet)
-		
-	}
-	return bets
-}
 
 // StartClient sends message to the server and wait for the response
 func (c *Client) StartClient() {
@@ -261,8 +160,26 @@ func (c *Client) StartClient() {
 	c.createClientSocket()
 
 	bets := c.readBetsFromFile(csvReader, c.config.BatchMaxAmount)
-	
 
+	c.sendBetsAndReceiveAck(bets)
+
+	winners, err := c.receiveWinners()
+	if err != nil {
+		c.StopClient()
+		return
+	}
+	msg, err := c.readMsg(len(SERVER_ACK + "\n"))
+	if err != nil || msg != SERVER_ACK+"\n" {
+		c.StopClient()
+		return
+	}
+	log.Infof("action: consulta_ganadores | result: success | cant_ganadores: %v", len(winners))
+
+	c.conn.Close()
+	c.conn_closed = true
+}
+
+func (c *Client) sendBetsAndReceiveAck(bets []string) {
 	for len(bets) > 0 {
 		batchToSend, bytesToSend, err := c.prepareBatchForSending(bets)
 		if err != nil || bytesToSend == nil {
@@ -285,21 +202,6 @@ func (c *Client) StartClient() {
 
 		bets = c.readBetsFromFile(csvReader, c.config.BatchMaxAmount)
 	}
-
-	winners, err := c.receiveWinners()
-	if err != nil {
-		c.StopClient()
-		return
-	}
-	msg, err := c.readMsg(len(SERVER_ACK + "\n"))
-	if err != nil || msg != SERVER_ACK+"\n" {
-		c.StopClient()
-		return
-	}
-	log.Infof("action: consulta_ganadores | result: success | cant_ganadores: %v", len(winners))
-
-	c.conn.Close()
-	c.conn_closed = true
 }
 
 func (c *Client) StopClient() {
---
+++ client/common/client_batch.go
@@ -0,0 +1,83 @@
+package common
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"strings"
+)
+
+
+func (c *Client) serialize_batch(bets []Bet, eof_flag uint8) ([]byte, error) {
+	var buffer bytes.Buffer
+	for _, bet := range bets {
+		betBuffer, err := bet.serialize()
+		if err != nil {
+			log.Criticalf("action: serialize | result: fail | client_id: %v | error: %v",
+				c.config.ID,
+				err,
+			)
+			return nil, err
+		}
+		if _, err := buffer.Write(betBuffer); err != nil {
+			return nil, err
+		}
+	}
+	totalLength := uint8(len(bets))
+	var finalBuffer bytes.Buffer
+	binary.Write(&finalBuffer, binary.BigEndian, eof_flag)
+
+	if err := binary.Write(&finalBuffer, binary.BigEndian, totalLength); err != nil {
+        return nil, err
+    }
+
+	finalBuffer.Write(buffer.Bytes())
+	return finalBuffer.Bytes(), nil
+}
+
+func (c *Client) prepareBatchForSending(bets []Bet) ([]Bet, []byte, error) {
+
+	batch := chunkBets(bets, c.config.BatchMaxAmount)
+
+	buffer, err := c.serialize_batch(batch, func() uint8 {
+		if len(batch) == len(bets) {
+			return EOF_MSG_TRUE
+		}
+		return EOF_MSG_FALSE
+	}())
+		
+
+	if err != nil {
+		return batch, nil, err
+	}
+
+	for len(buffer) > BATCH_MAX_AMOUNT_BYTES {
+		batchSize := len(batch) * 3 / 4
+		if batchSize < 1 {
+			return nil, nil, fmt.Errorf("batch size too small to continue")
+		}
+		batch = chunkBets(bets, batchSize)
+		if len(batch) == len(bets) {
+			buffer, err = c.serialize_batch(batch, EOF_MSG_TRUE)
+		} else {	
+			buffer, err = c.serialize_batch(batch, EOF_MSG_FALSE)
+		}
+		if err != nil {
+			return batch, nil, err
+		}
+	}
+	return batch, buffer, nil
+}
+
+func chunkBets(bets []Bet, maxAmount int) []Bet {
+	batches := make([]Bet, 0, maxAmount)
+
+	if len(bets) <= maxAmount {
+
+		return bets
+	}
+	for i := 0; i < maxAmount; i++ {
+		batches = append(batches, bets[i])
+	}
+	return batches
+}
